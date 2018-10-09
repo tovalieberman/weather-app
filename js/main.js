ef8d26820b78eab50a21f42faaa09897a5e042a5
Tova Lieberman <Tova_Lieberman1@glic.com>
2018-10-09 16:31:51 -0400

    Added background images for different types of weather

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $('#form').on('submit', function() {
     console.log('clicked submit');
     let location = $('#location').val();
-    $('.response').html("You entered " + location);
+    let weatherList = ["sunny", "cloudy", "rainy", "stormy"];
+    let weatherRand = Math.floor(Math.random() * weatherList.length);
+    let weather = weatherList[weatherRand];
+    $('.response').html(`You entered ${location}. The weather in ${location} is ${weather}`);
+    $('#weather-background').removeClass();
+    $('#weather-background').addClass(weather);
     return false;
 });
