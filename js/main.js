37be930d763582151a73bafdceb4a91db3752382
Tova Lieberman <Tova_Lieberman1@glic.com>
2018-10-10 16:42:18 -0400

    Fixes for how the data from the API is output + refactored JS functions

--- js/main.js
+++ js/main.js
@@ -1,46 +1,95 @@
-$('#form').on('submit', function() {
+$('#form').on('submit', function () {
     console.log('clicked submit');
-    let location = $('#location').val();
-    let weatherList = ["Sunny", "Cloudy", "Rainy", "Stormy", "Clear", "Snowy", "Foggy"];
-    let weatherRand = Math.floor(Math.random() * weatherList.length);
-    let weather = weatherList[weatherRand];
-    let message = getCustomMessage(weather);
-    $('.response').html(`<h3 class="location">${location}</h3><h2 class="weather">${weather}</h2>`);
-    $('#weather-background').removeClass();
-    $('#weather-background').addClass(weather.toLowerCase());
-    $('.current-weather').removeClass("hidden");
-    $('.forecast').removeClass("hidden");
     callAPI();
+    // let weather = getWeatherStatus();
+    // updateBackgroundImage();
     return false;
 });
 
+
+
 let units = "imperial"; //switch to metric for celsius
 let apiKey = "5a37ff291870ef42192074fc8420c55a";
-let query = "London,UK";
+let query = "New York";
 let action = "weather"; //switch to forecast for 5-day forecast
 
 function callAPI() {
     $.ajax({
-        type: "GET",
+        Category: "GET",
         url: `https://api.openweathermap.org/data/2.5/${action}?q=${query}&appid=${apiKey}&units=${units}`,
-        success: displayOutput,
-        error: function(data, status, errorThrown) {
-          console.log('error code ' + status);
+        success: handleResponse,
+        error: function (data, status, errorThrown) {
+            console.log('error code ' + status);
         }
     });
 }
 
+function handleResponse(response) {
+    displayOutput(response);
+    let weatherStatus = getWeatherStatus(response);
+    updateBackgroundImage(weatherStatus);
+}
+
 function displayOutput(resp) {
     console.log(resp);
-    let nameH2 = $('<h2>').html(resp.name);
-    $('.response').append(nameH2);
-    let weatherH2 = $('<h2>').html(resp.weather[0].main);
-    $('.response').append(weatherH2);
+    $('.location').html(resp.name);
+    $('.weather').html(resp.weather[0].main);
 
-    $('#temp-value').html(Math.round(resp.main.temp));
+    $('#temp-value').html(Math.round(resp.main.temp) + "&#176;");
     $('#humidity-value').html(resp.main.humidity);
     $('#pressure-value').html(resp.main.pressure);
     $('#wind-speed-value').html(Math.round(resp.wind.speed));
+}
+
+function getWeatherStatus(response) {
+    let statusCode = response.weather[0].id + ""; //cast to string
+    console.log(statusCode);
+    updateBackgroundImage(statusCode);
+
+    //First character indicates the Category of weather. Source: https://openweathermap.org/weather-conditions
+    let weatherCategory = "";
+    switch (statusCode[0]) {
+        case "2":
+            weatherCategory = "stormy"; //in their words: thunderstorm
+            break;
+        case "3":
+            weatherCategory = "cloudy"; //in their words: drizzle - TODO: find drizzle image
+            break;
+        case "5":
+            weatherCategory = "rainy";
+            break;
+        case "6":
+            weatherCategory = "snowy";
+            break;
+        case "7":
+            weatherCategory = "foggy"; //in their words: atmosphere
+            break;
+        case "8":
+            weatherCategory = "clear"; //TODO: sunny category?
+            break;
+        default:
+            weatherCategory = "unkown";
+            break;
+    }
+
+    //Edge case: 80x group: Clouds
+    if (statusCode === "801" 
+     || statusCode === "802"
+     || statusCode === "803"
+     || statusCode === "804") {
+        weatherCategory = "cloudy";
+    }
+
+    console.log(weatherCategory);
+
+    return weatherCategory;
+}
+
+function updateBackgroundImage(weatherCategory) {
+    $('#weather-background').removeClass();
+    $('#weather-background').addClass(weatherCategory);
+    $('.current-weather').removeClass("hidden");
+    $('.forecast').removeClass("hidden");
 }
 
 function getCustomMessage(weather) {
@@ -54,13 +103,13 @@ function getCustomMessage(weather) {
             break;
         case "cloudy":
             message = "But a cloudy day is no match for a sunny disposition.";
-        break;
+            break;
         case "stormy":
             message = "If you want to see the sunshine, you have to weather the storm.";
-        break;
+            break;
         default:
             message = "Unknown weather status";
-        break;
+            break;
     }
     return message;
 }
